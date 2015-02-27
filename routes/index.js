2e8cc34c89a978a4454ed5ea73af3c5d241691be
Toni S <TeKanne774@gmail.com>
2015-02-27 18:54:17 +0100

    Replace fs.watch with chokidar

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var chokidar = require('chokidar');
 
 var router = express.Router();
 
 var wordsFile = path.join(__dirname, '../words.json');
 var words = JSON.parse(fs.readFileSync(wordsFile, 'utf8'));
 
-fs.watch(wordsFile, function(err) {
-	if (err) return console.log(err);
+
+chokidar.watch(wordsFile).on('change', function() {
 	fs.readFile(wordsFile, 'utf8', function(err, data) {
 		if (err) return console.log(err);
 		words = JSON.parse(data);
